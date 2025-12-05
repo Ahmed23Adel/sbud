1e91f18ad9f48ed9b291aa008b8077b3a487f2d7
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2025-12-05 10:27:47 +0100

    initializing the app

---
+++ sbud/sbud/sbudApp.swift
@@ -0,0 +1,17 @@
+//
+//  sbudApp.swift
+//  sbud
+//
+//  Created by ahmed on 05/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct sbudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
