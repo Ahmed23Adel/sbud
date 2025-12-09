0939bc513556062148dcee39daebcc1bd2b62318
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2025-12-09 11:22:52 +0100

    initializing firebase

--- sbud/sbud/sbudApp.swift
+++ sbud/sbud/sbudApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import FirebaseCore
+import GoogleSignIn
+
+
+// Note: Used to enable push notification in future
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+      FirebaseApp.configure()
+    return true
+  }
+}
 
 @main
 struct sbudApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .onOpenURL{ url in
+                    GIDSignIn.sharedInstance.handle(url)
+                }
         }
     }
 }
