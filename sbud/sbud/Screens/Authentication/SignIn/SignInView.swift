54da211afd32bd8f99c471648d549acaab11162c
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2025-12-10 11:24:09 +0100

    animatin google icon in sign in

--- sbud/sbud/Screens/Authentication/SignIn/SignInView.swift
+++ sbud/sbud/Screens/Authentication/SignIn/SignInView.swift
@@ -36,6 +36,7 @@ struct SignInView: View {
                     Image("ios_light_rd_na")
                         .resizable()
                         .frame(width: 50, height: 50)
+                        .popUp(delay: 0.3)
                         .shadow(color: .black.opacity(0.2), radius: 8, x: 0, y: 4)
                         .shadow(color: .black.opacity(0.1), radius: 2, x: 0, y: 2)
                 }
