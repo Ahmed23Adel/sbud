660783520aee4ed0d7685f48abffb55b834c09fb
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2026-01-04 00:55:02 +0100

    temporarly relocating the signout button

--- sbud/sbud/Screens/Home/HomeView.swift
+++ sbud/sbud/Screens/Home/HomeView.swift
@@ -8,8 +8,17 @@
 import SwiftUI
 
 struct HomeView: View {
+    let authManager = AuthenticationManager.shared
+    @EnvironmentObject var coordinator: MainCoordinator
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Button {
+            Task {
+                try await authManager.signOut()
+                coordinator.goToSignUp()
+            }
+        } label: {
+            Text("sign out")
+        }
     }
 }
 
