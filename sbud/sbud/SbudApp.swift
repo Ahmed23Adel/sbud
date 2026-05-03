fdf3dd327eb353247ebd5fd6c1988ef2701e389b
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2026-05-03 22:50:43 +0200

    adding stubs for remote notifications; though not used as it needs paid account

--- sbud/sbud/SbudApp.swift
+++ sbud/sbud/SbudApp.swift
@@ -10,15 +10,32 @@ import FirebaseCore
 import GoogleSignIn
 import AdelsonAuthManager
 import AdelsonApiCaller
+import FirebaseMessaging
 
 // Note: Used to enable push notification in future
-class AppDelegate: NSObject, UIApplicationDelegate {
+class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
     
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
-      FirebaseApp.configure()
-    return true
-  }
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
+        FirebaseApp.configure()
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { _, _ in }
+        UIApplication.shared.registerForRemoteNotifications()
+        return true
+    }
+
+    func application(_ application: UIApplication,
+                     didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        Messaging.messaging().apnsToken = deviceToken   
+        Task {
+            await FCMExtractor().saveFCMToken()
+        }
+    }
+
+    func application(_ application: UIApplication,
+                     didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print("Failed to register for remote notifications: \(error)")
+    }
     
     
 }
