84d02e1d00fd7a18f6d832b542ac96921c746bea
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2026-05-05 23:17:54 +0200

    adding camer request

---
+++ sbud/sbud/Screens/Profile/Qrcode/ScanQRView.swift
@@ -0,0 +1,18 @@
+//
+//  ScanQRView.swift
+//  sbud
+//
+//  Created by ahmed on 05/05/2026.
+//
+
+import SwiftUI
+
+struct ScanQRView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScanQRView()
+}
---
+++ sbud/sbud/Screens/Profile/Qrcode/ShowQRView.swift
@@ -0,0 +1,18 @@
+//
+//  ShowQRView.swift
+//  sbud
+//
+//  Created by ahmed on 05/05/2026.
+//
+
+import SwiftUI
+
+struct ShowQRView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ShowQRView()
+}
