d47f96a18aab809fe95e0bd85dd5122e4c927b72
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2026-05-22 01:51:15 +0200

    showing no hosts for no friends case

--- sbud/sbud/Screens/Profile/MyEvents/Hosts/ViewHosts.swift
+++ sbud/sbud/Screens/Profile/MyEvents/Hosts/ViewHosts.swift
@@ -19,6 +19,8 @@ struct ViewHosts: View {
 
             if viewModel.isLoading {
                 ProgressView().tint(Color.mainColor)
+            } else if viewModel.friendHostItems.count == 0 {
+                Text("No friends available")
             } else {
                 ScrollView(showsIndicators: false) {
                     VStack(alignment: .leading, spacing: 28) {
