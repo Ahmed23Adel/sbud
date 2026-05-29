ec20b1c458ac44fb66d1d1dbd6b93c167a822dc8
Ahmed23Adel <ahmed.hussein.1521@gmail.com>
2026-05-29 19:19:50 +0200

    Fix merge issues

--- sbud/sbud/Screens/Profile/Components/Components.swift
+++ sbud/sbud/Screens/Profile/Components/Components.swift
@@ -26,3 +26,23 @@ func customTextField(title: String, placeholder: String, text: Binding<String>) 
         .cornerRadius(4)
     }
 }
+
+extension UIApplication {
+    static var safeAreaTop: CGFloat {
+        shared.connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .first?.windows.first?.safeAreaInsets.top ?? 0
+    }
+
+    static var safeAreaBottom: CGFloat {
+        shared.connectedScenes
+            .compactMap { $0 as? UIWindowScene }
+            .first?.windows.first?.safeAreaInsets.bottom ?? 0
+    }
+}
+
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
